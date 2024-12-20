83dc87175c8eb15e250adeffed8ea4d6f415861b
pavan7297 <mallapavan072@gmail.com>
2024-12-20 23:45:55 +0530

    lat long updated

--- ui/src/app/component/const/header/header.component.ts
+++ ui/src/app/component/const/header/header.component.ts
@@ -5,54 +5,58 @@ import { GeolocationService } from '../../../service/service/service/geolocation
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule,RouterLink,RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   isMenuOpen = false;
 
   latitude: number | null = null;
   longitude: number | null = null;
   errorMessage: string = '';
 
-  constructor(private router: Router,private geolocationService: GeolocationService){
-
-  }
+  constructor(
+    private router: Router,
+    private geolocationService: GeolocationService
+  ) {}
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  gotologin(){
+  gotologin() {
     this.router.navigate([`/login`]);
-
   }
 
-
-
-
-
   ngOnInit() {
-    this.getLocation();
-    console.log(JSON.stringify(this.getLocation()));
+    // this.getLocation();
+    // console.log(JSON.stringify(this.getLocation()));
   }
 
   // Function to get current location
   getLocation() {
-    this.geolocationService.getCurrentLocation()
-      .then((coords) => {
-        // Check if coords is valid
-        if (coords && coords.latitude && coords.longitude) {
-          this.latitude = coords.latitude;
-          this.longitude = coords.longitude;
-        } else {
-          this.errorMessage = 'Invalid coordinates received';
-        }
-      })
-      .catch((error) => {
-        this.errorMessage = 'Error: ' + (error instanceof Error ? error.message : error);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        localStorage.setItem('latitude',JSON.stringify(this.latitude))
+        localStorage.setItem('longitude',JSON.stringify(this.longitude))
       });
- 
+    }
+
+    // this.geolocationService.getCurrentLocation()
+    //   .then((coords) => {
+    //     // Check if coords is valid
+    //     if (coords && coords.latitude && coords.longitude) {
+    //       this.latitude = coords.latitude;
+    //       this.longitude = coords.longitude;
+    //     } else {
+    //       this.errorMessage = 'Invalid coordinates received';
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     this.errorMessage = 'Error: ' + (error instanceof Error ? error.message : error);
+    //   });
   }
 }
